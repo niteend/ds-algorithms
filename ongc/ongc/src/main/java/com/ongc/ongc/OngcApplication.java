fcaa124be602d39730b683d915565406c828a81e
ndhule1988 <java.dev1988@outlook.com>
2020-02-05 00:30:34 +0530

    Checking in whole project - ongc gradle project

---
+++ ongc/ongc/src/main/java/com/ongc/ongc/OngcApplication.java
@@ -0,0 +1,13 @@
+package com.ongc.ongc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OngcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OngcApplication.class, args);
+	}
+
+}
---
+++ ongc/ongc/src/test/java/com/ongc/ongc/OngcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ongc.ongc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OngcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
